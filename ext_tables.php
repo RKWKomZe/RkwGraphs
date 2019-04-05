ca7e6abd52f76c1e046a20dc47fe765b114fcc6c
Christian Dilger <c.dilger@addorange.de>
2019-04-05 15:43:45 +0200

    [FEATURE] added csh help texts to flexform

--- ext_tables.php
+++ ext_tables.php
@@ -12,13 +12,25 @@ call_user_func(
         );
 
         $pluginSignature = str_replace('_', '', $extKey) . '_graphs';
+
         $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $extKey . '/Configuration/FlexForms/flexform_graphs.xml');
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'RKW Graphs');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+            $pluginSignature,
+            'FILE:EXT:' . $extKey . '/Configuration/FlexForms/flexform_graphs.xml'
+        );
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwgraphs_domain_model_graphs', 'EXT:rkw_graphics/Resources/Private/Language/locallang_csh_tx_rkwgraphics_domain_model_graphs.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwgraphics_domain_model_graphs');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript', 'RKW Graphs'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tt_content.pi_flexform.' . $pluginSignature . '.list',
+            'EXT:' . $extKey . '/Resources/Private/Language/locallang_csh_tx_rkwgraphs.xlf'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwgraphs_domain_model_graphs');
 
     },
     $_EXTKEY
